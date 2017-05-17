d2065e71f1a75ca2c641d8cf06f8ef27dbad0cfa
user1990 <paulius.rimg1990@gmail.com>
2017-05-17 13:44:17 +0300

    Created .map example

---
+++ Functional Programming/#2 Map.js
@@ -0,0 +1,30 @@
+// Task: Get all names of all the animals
+var animals = [
+  { name: 'Fluffykins', species: 'rabbit' },
+  { name: 'Caro',       species: 'dog' },
+  { name: 'Hamilton',   species: 'dog' },
+  { name: 'Harold',     species: 'fish' },
+  { name: 'Ursula',     species: 'cat' },
+  { name: 'Jimmy',      species: 'fish' }
+];
+
+// 5 different methods to do this
+// for loop
+var names = [];
+for (var i = 0; i < animals.length; i++) {
+  names.push(animals[i].name);
+}
+
+// map-basic
+var names = animals.map(function(animal) { return animal.name; });
+
+// map-new-object
+var names = animals.map(function(animal) { return animal.name + ' the ' + animal.species; });
+
+// map-basic-arrow
+var names = animals.map((animal) => animal.name);
+
+// map-basic-arrow-x
+var names = animals.map((x) => x.name);
+
+
