79177573b70f5bc314a2226ab233f6c395b38bba
user1990 <paulius.rimg1990@gmail.com>
2017-05-17 14:01:33 +0300

    Created .reduce example

---
+++ Functional Programming/#3 Reduce.js
@@ -0,0 +1,15 @@
+var orders = [
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 },
+  { amount: 325 }
+];
+
+var totalAmount = orders.reduce(function(sum, order) {
+  return sum + order.amount;
+}, 0);
+
+console.log(totalAmount);
+
+
+
