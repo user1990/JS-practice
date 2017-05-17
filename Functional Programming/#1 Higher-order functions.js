899027e99c85c1d0e6bf49aaf7a68c6a7c25d4d6
user1990 <paulius.rimg1990@gmail.com>
2017-05-17 11:37:41 +0300

    HOF example

--- Functional Programming/#1 Higher-order functions.js
+++ Functional Programming/#1 Higher-order functions.js
@@ -1,1 +1,16 @@
+var cars = [
+  { 'name': 'Ford', 'model': 'Fiesta' },
+  { 'name': 'BMW', 'model': '320' },
+  { 'name': 'Fiat', 'model': '500' }
+];
+
+var isFiesta = function(car) {
+  return car.model === 'Fiesta';
+};
+
+var models = cars.filter(isFiesta);
+var otherCars = cars.reject(isFiesta);
+
+
+
 
